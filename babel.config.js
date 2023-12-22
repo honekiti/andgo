1da761cc484e539358b9d922a8574260acbea49f
Toshihide Hara <toshihidehr@gmail.com>
2023-12-22 20:36:49 +0900

    bump to expo sdk v50

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,7 @@
-module.exports = function (api) {
+module.exports = (api) => {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      // Required for expo-router
-      'expo-router/babel',
-    ],
+    plugins: [],
   };
 };
