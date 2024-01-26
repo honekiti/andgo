d2b3b3487c37d4b7dfbbc142fe81a98618e3fd8e
Toshihide Hara <toshihidehr@gmail.com>
2024-01-26 14:18:58 +0900

    update exchange-registration

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # hongo-v3
 
+UNDER CONSTRUCTION.
+
 ## Develop
 
 ```bash
 npm install
+# iOSでの実行の場合
 npm run dev:ios
+# Androidでの実行の場合
+npm run dev:android
 ```
--- src/app/(exchange)/exchange-registration.tsx
+++ src/app/(exchange)/exchange-registration.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   Box,
   VStack,
@@ -18,17 +19,26 @@ import {
   ButtonText,
   FormControl,
   FormControlLabel,
+  FormControlLabelText,
   Input,
   InputField,
 } from '@gluestack-ui/themed';
 
 export default function ExchangeRegistrationScreen() {
+  const [exchanges, setExchanges] = useState([
+    { id: 'bitflyer', name: 'bitFlyer' },
+    { id: 'coincheck', name: 'Coincheck' },
+  ]);
+  const [selectedExchangeId, setSelectedExchangeId] = useState<string | undefined>(undefined);
+
   return (
     <Box h="$full" w="$full">
-      <VStack>
+      <VStack space="lg">
         <FormControl size="md" isRequired={true}>
-          <FormControlLabel>取引所</FormControlLabel>
-          <Select>
+          <FormControlLabel>
+            <FormControlLabelText>取引所</FormControlLabelText>
+          </FormControlLabel>
+          <Select onValueChange={(v) => setSelectedExchangeId(v)}>
             <SelectTrigger variant="outline" size="md">
               <SelectInput placeholder="選択してください" />
               <SelectIcon mr="$3" as={ChevronDownIcon} />
@@ -40,19 +50,27 @@ export default function ExchangeRegistrationScreen() {
               <SelectDragIndicatorWrapper>
                 <SelectDragIndicator />
               </SelectDragIndicatorWrapper>
+
+              {exchanges.map((exchange) => (
+                <SelectItem key={exchange.id} label={exchange.name} value={exchange.id} />
+              ))}
             </SelectContent>
           </SelectPortal>
         </FormControl>
 
         <FormControl size="md" isRequired={true}>
-          <FormControlLabel>APIキー</FormControlLabel>
+          <FormControlLabel>
+            <FormControlLabelText>APIキー</FormControlLabelText>
+          </FormControlLabel>
           <Input>
             <InputField placeholder="発行したAPIキーを入力" />
           </Input>
         </FormControl>
 
         <FormControl size="md" isRequired={true}>
-          <FormControlLabel>APIシークレット</FormControlLabel>
+          <FormControlLabel>
+            <FormControlLabelText>APIシークレット</FormControlLabelText>
+          </FormControlLabel>
           <Input>
             <InputField placeholder="発行したAPIシークレットを入力" />
           </Input>
