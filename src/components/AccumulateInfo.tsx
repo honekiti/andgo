c44eb1e673a6e922f2e44cb903fbe17eedb32228
KIS000 <kisuuuu0@gmail.com>
2024-02-07 13:59:11 +0900

    機能追加

--- src/components/AccumulateInfo.tsx
+++ src/components/AccumulateInfo.tsx
@@ -1,20 +1,34 @@
 import { useState, useCallback } from 'react';
 import { useFocusEffect } from '@react-navigation/native';
-import { Box, Button, ButtonText, CheckCircleIcon, CloseCircleIcon, HStack, Icon, LinkText, ScrollView, Text, VStack } from '@gluestack-ui/themed';
+import { Box, Button, ButtonText, ScrollView, Text } from '@gluestack-ui/themed';
 import { white, unclearWhite, darkGrey, lightGrey, red, green } from '../constants/Colors';
 import { Link } from 'expo-router';
 import { genId } from '../utils/crypto';
 import { loadSchedules, saveScheduels } from '../services/schedule-service';
-import { Schedule } from '../models';
+import { ExchangeCredential, Schedule } from '../models';
+import ScheduleList from './ScheduleList';
+import { loadCredentials } from '../services/exchange-credential-service';
 
 export default function AccumulateInfo() {
   const [schedules, setSchedules] = useState<Schedule[]>([]);
 
   // このコンポーネントを含む画面にフォーカスが当たった際にスケジュール情報を読み込む
+  // ↓の「スケジュール情報」のlengthを使用すると、連携した直後の場面で「連携しよう」画面が表示されてしまうので「credentials」に取引所が入った段階で画面切り替えてます。
+  // useFocusEffect(
+  //   useCallback(() => {
+  //     loadSchedules().then((schedules) => {
+  //       setSchedules(schedules);
+  //     });
+  //   }, []),
+  // );
+
+  // 取引所連携情報読み込みを有効にしたら、初期値を空配列にする
+  const [credentials, setCredentials] = useState<ExchangeCredential[]>([]);
+  // 取引所連携情報を読み込む
   useFocusEffect(
     useCallback(() => {
-      loadSchedules().then((schedules) => {
-        setSchedules(schedules);
+      loadCredentials().then((credentials) => {
+        setCredentials(credentials);
       });
     }, []),
   );
@@ -27,187 +41,41 @@ export default function AccumulateInfo() {
         <Box h="$0.5" w="50%" bg={unclearWhite} rounded="$full" />
         <Box h="$0.5" w="50%" bg={white} rounded="$full" />
       </Box>
+
       {/* ↓ 取引所連携前 ↓ */}
-      {/* <Box h="auto" alignItems="center" my="$7">
-        <Box h="$20" w="$20" bg="#f003" rounded={'$full'} />
-        <Text mt="$2">暗号資産(仮想通貨)取引所と</Text>
-        <Text>連携しましょう</Text>
-      </Box>
+      {credentials.length === 0 && (
+        <ScrollView>
+          <Box h="auto" alignItems="center" my="$7">
+            <Box h="$20" w="$20" bg="#f003" rounded={'$full'} />
+            <Text mt="$2">暗号資産(仮想通貨)取引所と</Text>
+            <Text>連携しましょう</Text>
+          </Box>
 
-      <Box h="auto" alignItems="center" mb="$7">
-        <Link href="/exchange-registration" asChild>
-          <Button
-            h="$12"
-            w="90%"
-            mb="$5"
-            size="md"
-            variant="outline"
-            action="secondary"
-            isDisabled={false}
-            isFocusVisible={false}
-            borderWidth={2}
-            rounded="$lg"
-          >
-            <ButtonText color={white}>取引所と連携する</ButtonText>
-          </Button>
-        </Link>
-      </Box> */}
-      {/* ↑ 取引所連携前 ↑ */}
-      {/* ↓ 取引所連携後 ↓ */}
-      <Box h="auto" w="$full" justifyContent="space-between">
-        <ScrollView h="auto">
-          <VStack space="sm" p="$4">
-            <Link href="/schedule-edit" asChild>
-              <Button h="auto" w="100%" bg={lightGrey} rounded="$lg">
-                <HStack w="$80" justifyContent="space-between" alignItems="center" px="$2" py="$5">
-                  <Box>
-                    <HStack space="xs" alignItems="center">
-                      <Icon as={CheckCircleIcon} size="sm" color={green} />
-                      <Text color={white} fontSize={19} bold pb="$1">
-                        Kraken
-                      </Text>
-                    </HStack>
-                    <HStack space="xs">
-                      <Text color={unclearWhite} fontSize={13}>
-                        毎日
-                      </Text>
-                      <Text color={unclearWhite} fontSize={13}>
-                        -
-                      </Text>
-                      <Text color={unclearWhite} fontSize={13}>
-                        18:00
-                      </Text>
-                    </HStack>
-                  </Box>
-                  <VStack space="xs" alignItems="flex-end">
-                    <HStack>
-                      <Text color={white} fontSize={19}>
-                        1,000
-                      </Text>
-                      <Text color={white} fontSize={13} pl="$1">
-                        円
-                      </Text>
-                    </HStack>
-                    <HStack space="xs">
-                      <Text color={white} fontSize={11}>
-                        現在レートで＠
-                      </Text>
-                      <Text color={white} fontSize={11}>
-                        0.02300000
-                      </Text>
-                      <Text color={white} fontSize={11}>
-                        相当
-                      </Text>
-                    </HStack>
-                  </VStack>
-                </HStack>
+          <Box h="auto" alignItems="center" mb="$7">
+            <Link href="/exchange-registration" asChild>
+              <Button
+                h="$12"
+                w="90%"
+                mb="$5"
+                size="md"
+                variant="outline"
+                action="secondary"
+                isDisabled={false}
+                isFocusVisible={false}
+                borderWidth={2}
+                rounded="$lg"
+              >
+                <ButtonText color={white}>取引所と連携する</ButtonText>
               </Button>
             </Link>
-            <Link href="/schedule-edit" asChild>
-              <Button h="auto" w="100%" bg={lightGrey} rounded="$lg">
-                <HStack w="$80" justifyContent="space-between" alignItems="center" px="$2" py="$5">
-                  <Box>
-                    <HStack space="xs" alignItems="center">
-                      <Icon as={CheckCircleIcon} size="sm" color={green} />
-                      <Text color={white} fontSize={19} bold pb="$1">
-                        bitFlyer
-                      </Text>
-                    </HStack>
-                    <HStack space="xs">
-                      <Text color={unclearWhite} fontSize={13}>
-                        毎日
-                      </Text>
-                      <Text color={unclearWhite} fontSize={13}>
-                        -
-                      </Text>
-                      <Text color={unclearWhite} fontSize={13}>
-                        18:00
-                      </Text>
-                    </HStack>
-                  </Box>
-                  <VStack space="xs" alignItems="flex-end">
-                    <HStack>
-                      <Text color={white} fontSize={19}>
-                        20,000
-                      </Text>
-                      <Text color={white} fontSize={13} pl="$1">
-                        円
-                      </Text>
-                    </HStack>
-                    <HStack space="xs">
-                      <Text color={white} fontSize={11}>
-                        現在レートで＠
-                      </Text>
-                      <Text color={white} fontSize={11}>
-                        0.56700000
-                      </Text>
-                      <Text color={white} fontSize={11}>
-                        相当
-                      </Text>
-                    </HStack>
-                  </VStack>
-                </HStack>
-              </Button>
-            </Link>
-            <Link href="/schedule-edit" asChild>
-              <Button h="auto" w="100%" bg="#33333366" rounded="$lg">
-                <HStack w="$80" justifyContent="space-between" alignItems="center" px="$2" py="$5">
-                  <Box>
-                    <HStack space="xs" alignItems="center">
-                      <Icon as={CloseCircleIcon} size="sm" color={red} />
-                      <Text color={white} fontSize={19} bold pb="$1">
-                        GMOコイン
-                      </Text>
-                    </HStack>
-                    {/* <HStack space="xs">
-                      <Text color={unclearWhite} fontSize={13}>
-                        毎日
-                      </Text>
-                      <Text color={unclearWhite} fontSize={13}>
-                        -
-                      </Text>
-                      <Text color={unclearWhite} fontSize={13}>
-                        18:00
-                      </Text>
-                    </HStack> */}
-                    <Text>停止中</Text>
-                  </Box>
-                  <VStack space="xs" alignItems="flex-end">
-                    <HStack>
-                      <Text color={white} fontSize={19}>
-                        50,000
-                      </Text>
-                      <Text color={white} fontSize={13} pl="$1">
-                        円
-                      </Text>
-                    </HStack>
-                    <HStack space="xs">
-                      <Text color={white} fontSize={11}>
-                        現在レートで＠
-                      </Text>
-                      <Text color={white} fontSize={11}>
-                        0.78900000
-                      </Text>
-                      <Text color={white} fontSize={11}>
-                        相当
-                      </Text>
-                    </HStack>
-                  </VStack>
-                </HStack>
-              </Button>
-            </Link>
-          </VStack>
+            <Box h="$40" />
+          </Box>
         </ScrollView>
+      )}
+      {/* ↑ 取引所連携前 ↑ */}
 
-        <HStack justifyContent="space-between" alignItems="center" p="$4" mb="$3" borderTopWidth={0.3} borderColor={unclearWhite} bg={darkGrey}>
-          <Text color={white}>積立プランを追加しよう</Text>
-          <Link href="/schedule-registration" asChild>
-            <Button w="$24" size="lg" variant="solid" action="primary" isDisabled={false} isFocusVisible={false} rounded="$full">
-              <ButtonText>追加</ButtonText>
-            </Button>
-          </Link>
-        </HStack>
-      </Box>
+      {/* ↓ 取引所連携後 ↓ */}
+      {credentials.length > 0 && <ScheduleList schedules={schedules} />}
       {/* ↑ 取引所連携後 ↑ */}
     </>
   );
--- src/components/ScheduleList.tsx
+++ src/components/ScheduleList.tsx
@@ -1,7 +1,22 @@
 import { ListRenderItem } from 'react-native';
-import { Box, Heading, FlatList } from '@gluestack-ui/themed';
+import {
+  Box,
+  Text,
+  Heading,
+  FlatList,
+  ScrollView,
+  VStack,
+  Button,
+  HStack,
+  Icon,
+  CheckCircleIcon,
+  CloseCircleIcon,
+  ButtonText,
+} from '@gluestack-ui/themed';
 import { Schedule } from '../models';
 import ScheduleItem from './ScheduleItem';
+import { darkGrey, green, lightGrey, red, unclearWhite, white } from '../constants/Colors';
+import { Link } from 'expo-router';
 export type ScheduleListProps = {
   schedules: Schedule[];
 };
@@ -9,13 +24,154 @@ export default function ScheduleList(props: ScheduleListProps) {
   const renderItem: ListRenderItem<Schedule> = ({ item }) => <ScheduleItem item={item} />;
 
   return (
-    <Box py="$10">
-      <Heading size="xl" p="$4" pb="$3">
-        スケジュール
-      </Heading>
-      {/* type bug: https://github.com/gluestack/gluestack-ui/issues/1041 */}
-      {/* biome-ignore lint/suspicious/noExplicitAny: <explanation> */}
-      <FlatList data={props.schedules} renderItem={renderItem as any} keyExtractor={(item) => (item as Schedule).id} />
+    <Box>
+      <Box h="auto" w="$full" justifyContent="space-between">
+        <ScrollView h="auto">
+          {/* type bug: https://github.com/gluestack/gluestack-ui/issues/1041 */}
+          {/* biome-ignore lint/suspicious/noExplicitAny: <explanation> */}
+          {/* <FlatList data={props.schedules} renderItem={renderItem as any} keyExtractor={(item) => (item as Schedule).id} /> */}
+
+          <VStack space="sm" p="$4">
+            <Link href="/schedule-edit" asChild>
+              <Button h="auto" w="100%" bg={lightGrey} rounded="$lg">
+                <HStack w="$80" justifyContent="space-between" alignItems="center" px="$2" py="$5">
+                  <Box>
+                    <HStack space="xs" alignItems="center">
+                      <Icon as={CheckCircleIcon} size="sm" color={green} />
+                      <Text color={white} fontSize={19} bold pb="$1">
+                        Kraken
+                      </Text>
+                    </HStack>
+                    <HStack space="xs">
+                      <Text color={unclearWhite} fontSize={13}>
+                        毎日
+                      </Text>
+                      <Text color={unclearWhite} fontSize={13}>
+                        -
+                      </Text>
+                      <Text color={unclearWhite} fontSize={13}>
+                        18:00
+                      </Text>
+                    </HStack>
+                  </Box>
+                  <VStack space="xs" alignItems="flex-end">
+                    <HStack>
+                      <Text color={white} fontSize={19}>
+                        1,000
+                      </Text>
+                      <Text color={white} fontSize={13} pl="$1">
+                        円
+                      </Text>
+                    </HStack>
+                    <HStack space="xs">
+                      <Text color={white} fontSize={11}>
+                        現在レートで＠
+                      </Text>
+                      <Text color={white} fontSize={11}>
+                        0.02300000
+                      </Text>
+                      <Text color={white} fontSize={11}>
+                        相当
+                      </Text>
+                    </HStack>
+                  </VStack>
+                </HStack>
+              </Button>
+            </Link>
+            <Link href="/schedule-edit" asChild>
+              <Button h="auto" w="100%" bg={lightGrey} rounded="$lg">
+                <HStack w="$80" justifyContent="space-between" alignItems="center" px="$2" py="$5">
+                  <Box>
+                    <HStack space="xs" alignItems="center">
+                      <Icon as={CheckCircleIcon} size="sm" color={green} />
+                      <Text color={white} fontSize={19} bold pb="$1">
+                        bitFlyer
+                      </Text>
+                    </HStack>
+                    <HStack space="xs">
+                      <Text color={unclearWhite} fontSize={13}>
+                        毎日
+                      </Text>
+                      <Text color={unclearWhite} fontSize={13}>
+                        -
+                      </Text>
+                      <Text color={unclearWhite} fontSize={13}>
+                        18:00
+                      </Text>
+                    </HStack>
+                  </Box>
+                  <VStack space="xs" alignItems="flex-end">
+                    <HStack>
+                      <Text color={white} fontSize={19}>
+                        20,000
+                      </Text>
+                      <Text color={white} fontSize={13} pl="$1">
+                        円
+                      </Text>
+                    </HStack>
+                    <HStack space="xs">
+                      <Text color={white} fontSize={11}>
+                        現在レートで＠
+                      </Text>
+                      <Text color={white} fontSize={11}>
+                        0.56700000
+                      </Text>
+                      <Text color={white} fontSize={11}>
+                        相当
+                      </Text>
+                    </HStack>
+                  </VStack>
+                </HStack>
+              </Button>
+            </Link>
+            <Link href="/schedule-edit" asChild>
+              <Button h="auto" w="100%" bg="#33333366" rounded="$lg">
+                <HStack w="$80" justifyContent="space-between" alignItems="center" px="$2" py="$5">
+                  <Box>
+                    <HStack space="xs" alignItems="center">
+                      <Icon as={CloseCircleIcon} size="sm" color={red} />
+                      <Text color={white} fontSize={19} bold pb="$1">
+                        GMOコイン
+                      </Text>
+                    </HStack>
+                    <Text>停止中</Text>
+                  </Box>
+                  <VStack space="xs" alignItems="flex-end">
+                    <HStack>
+                      <Text color={white} fontSize={19}>
+                        50,000
+                      </Text>
+                      <Text color={white} fontSize={13} pl="$1">
+                        円
+                      </Text>
+                    </HStack>
+                    <HStack space="xs">
+                      <Text color={white} fontSize={11}>
+                        現在レートで＠
+                      </Text>
+                      <Text color={white} fontSize={11}>
+                        0.78900000
+                      </Text>
+                      <Text color={white} fontSize={11}>
+                        相当
+                      </Text>
+                    </HStack>
+                  </VStack>
+                </HStack>
+              </Button>
+            </Link>
+          </VStack>
+        </ScrollView>
+
+        <HStack justifyContent="space-between" alignItems="center" p="$4" mb="$3" borderTopWidth={0.3} borderColor={unclearWhite} bg={darkGrey}>
+          <Text color={white}>積立プランを追加しよう</Text>
+          <Link href="/schedule-registration" asChild>
+            <Button w="$24" size="lg" variant="solid" action="primary" isDisabled={false} isFocusVisible={false} rounded="$full">
+              <ButtonText>追加</ButtonText>
+            </Button>
+          </Link>
+        </HStack>
+      </Box>
     </Box>
   );
 }
