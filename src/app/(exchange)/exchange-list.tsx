ea9eaa825359b2238a13c226f90f4f1f66946216
Toshihide Hara <toshihidehr@gmail.com>
2024-02-10 16:40:01 +0900

    add handlePressUnregister

--- src/app/(exchange)/exchange-list.tsx
+++ src/app/(exchange)/exchange-list.tsx
@@ -1,11 +1,12 @@
 import { useState, useCallback } from 'react';
 import { ListRenderItem } from 'react-native';
-import { Box, Button, FlatList, HStack, VStack, Text, Icon, ButtonText, GripVerticalIcon } from '@gluestack-ui/themed';
+import { Box, Button, FlatList, HStack, VStack, Text, Icon, ButtonText, GripVerticalIcon, useToast, Toast, ToastTitle } from '@gluestack-ui/themed';
 import { Link, useFocusEffect } from 'expo-router';
 import { loadCredentials } from '../../services/exchange-credential-service';
 import { EXCHANGES } from '../../master';
-import { ExchangeCredential } from '../../models';
+import { ExchangeCredential, ExchangeId } from '../../models';
 import { white, unclearWhite, darkGrey } from '../../constants/Colors';
+import { saveCredentials } from '../../services/exchange-credential-service';
 
 export type ExchangeInfo = {
   name: string;
@@ -13,12 +14,27 @@ export type ExchangeInfo = {
 };
 
 export default function ExchangeListScreen() {
+  const toast = useToast();
   const [credentials, setCredentials] = useState<ExchangeCredential[]>([]);
   const [balances, setBalances] = useState<(number | undefined)[]>([]);
   const items: ExchangeInfo[] = credentials.map((credential, index) => ({
     name: EXCHANGES.find((ex) => ex.id === credential.id)?.name ?? 'unknown',
     balance: balances[index],
   }));
+
+  const handlePressUnregister = (id: ExchangeId) => {
+    const newCredentials = credentials.filter((c) => c.id !== id);
+    saveCredentials(newCredentials);
+    setCredentials(newCredentials);
+
+    toast.show({
+      render: () => (
+        <Toast action="success">
+          <ToastTitle>連携を解除しました</ToastTitle>
+        </Toast>
+      ),
+    });
+  };
 
   const renderItem: ListRenderItem<ExchangeInfo> = ({ item }) => {
     return (
