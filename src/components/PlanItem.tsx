5dee91c80c46bd54bffa0e4cfe69d7e5bd03645d
KIS000 <kisuuuu0@gmail.com>
2024-02-23 18:08:47 +0900

    update-plansUI

--- src/components/PlanItem.tsx
+++ src/components/PlanItem.tsx
@@ -1,14 +1,95 @@
-import { Box, Text } from '@gluestack-ui/themed';
+import { Box, Button, CheckCircleIcon, CloseCircleIcon, HStack, Icon, Text, VStack, Image } from '@gluestack-ui/themed';
 import { Plan } from '../models';
+import { Link } from 'expo-router';
+import { green, lightGrey, red, unclearWhite, white, btnFalse } from '../constants/Colors';
+import { getExchange } from '../services/exchange-service';
+import { getPlanType, getRefAtDetails } from '../services/plan-service';
+import { DAY_OF_WEEK_OPTIONS } from '../master';
 
 export type PlanItemProps = {
   item: Plan;
 };
 
 export default function PlanItem(props: PlanItemProps) {
+  const exchange = getExchange(props.item.exchangeId);
+  const planType = getPlanType(props.item.planTypeId);
+  const refAtDetails = getRefAtDetails(props.item);
+  const btnColor = props.item.status.enabled ? lightGrey : btnFalse;
   return (
     <Box justifyContent="center">
-      <Text>{JSON.stringify(props.item)}</Text>
+      <Link href="/schedule-edit" asChild>
+        <Button h="auto" w="100%" bg={btnColor} rounded="$lg" mb="$2">
+          <HStack w="$80" justifyContent="space-between" alignItems="center" px="$2" py="$5">
+            <Box>
+              <HStack space="xs" alignItems="center">
+                {props.item.status.enabled ? (
+                  <Icon as={CheckCircleIcon} size="sm" color={green} />
+                ) : (
+                  <Icon as={CloseCircleIcon} size="sm" color={red} />
+                )}
+                <Text color={white} fontSize={19} bold pb="$1">
+                  {exchange.name}
+                </Text>
+              </HStack>
+              {props.item.status.enabled ? (
+                <HStack space="xs">
+                  <Text color={unclearWhite} fontSize={13}>
+                    {planType.name}
+                  </Text>
+                  {props.item.planTypeId !== 'DAILY' && (
+                    <Text color={unclearWhite} fontSize={13}>
+                      {props.item.planTypeId === 'WEEKLY' && DAY_OF_WEEK_OPTIONS[refAtDetails.dayOfWeek].label}
+                      {props.item.planTypeId === 'MONTHLY' && refAtDetails.date}
+                      {props.item.planTypeId === 'MONTHLY' && (
+                        <Text color={unclearWhite} fontSize={13}>
+                          日
+                        </Text>
+                      )}
+                    </Text>
+                  )}
+                  <Text color={unclearWhite} fontSize={13}>
+                    -
+                  </Text>
+                  <Text color={unclearWhite} fontSize={13}>
+                    ????
+                  </Text>
+                </HStack>
+              ) : (
+                <Text>停止中</Text>
+              )}
+            </Box>
+            <VStack space="xs" alignItems="flex-end">
+              <HStack>
+                <Text color={white} fontSize={19}>
+                  {JSON.stringify(props.item.quoteAmount)}
+                </Text>
+                <Text color={white} fontSize={13} pl="$1">
+                  円
+                </Text>
+              </HStack>
+              <HStack space="xs" alignItems="center">
+                <Text color={white} fontSize={11}>
+                  現在レートで
+                </Text>
+                <Image
+                  h="$4"
+                  w="$4"
+                  bg="#0000"
+                  resizeMode="contain"
+                  source={require('../../assets/images/bit-coin-line.png')}
+                  alt="bit-coin-line-logo"
+                />
+                <Text color={white} fontSize={11}>
+                  ????
+                </Text>
+                <Text color={white} fontSize={11}>
+                  相当
+                </Text>
+              </HStack>
+            </VStack>
+          </HStack>
+        </Button>
+      </Link>
     </Box>
   );
 }
--- src/components/PlanList.tsx
+++ src/components/PlanList.tsx
@@ -4,7 +4,7 @@ import { Box, Text, FlatList, ScrollView, VStack, Button, HStack, Icon, CheckCir
 import { Plan } from '../models';
 import PlanItem from './PlanItem';
 import { plansAtom } from '../services/plan-service';
-import { darkGrey, green, lightGrey, red, unclearWhite, white } from '../constants/Colors';
+import { darkGrey, unclearWhite, white } from '../constants/Colors';
 import { Link } from 'expo-router';
 
 export default function PlanList() {
@@ -12,153 +12,19 @@ export default function PlanList() {
   const renderItem: ListRenderItem<Plan> = ({ item }) => <PlanItem item={item} />;
 
   return (
-    <Box>
+    <Box h="100%" w="$full" justifyContent="space-between">
       {/* type bug: https://github.com/gluestack/gluestack-ui/issues/1041 */}
       {/* biome-ignore lint/suspicious/noExplicitAny: <explanation> */}
-      <FlatList data={plans} renderItem={renderItem as any} keyExtractor={(item) => (item as Plan).id} />
-      <Box h="auto" w="$full" justifyContent="space-between">
-        <ScrollView h="auto">
-          <VStack space="sm" p="$4">
-            <Link href="/schedule-edit" asChild>
-              <Button h="auto" w="100%" bg={lightGrey} rounded="$lg">
-                <HStack w="$80" justifyContent="space-between" alignItems="center" px="$2" py="$5">
-                  <Box>
-                    <HStack space="xs" alignItems="center">
-                      <Icon as={CheckCircleIcon} size="sm" color={green} />
-                      <Text color={white} fontSize={19} bold pb="$1">
-                        Kraken
-                      </Text>
-                    </HStack>
-                    <HStack space="xs">
-                      <Text color={unclearWhite} fontSize={13}>
-                        毎日
-                      </Text>
-                      <Text color={unclearWhite} fontSize={13}>
-                        -
-                      </Text>
-                      <Text color={unclearWhite} fontSize={13}>
-                        18:00
-                      </Text>
-                    </HStack>
-                  </Box>
-                  <VStack space="xs" alignItems="flex-end">
-                    <HStack>
-                      <Text color={white} fontSize={19}>
-                        1,000
-                      </Text>
-                      <Text color={white} fontSize={13} pl="$1">
-                        円
-                      </Text>
-                    </HStack>
-                    <HStack space="xs">
-                      <Text color={white} fontSize={11}>
-                        現在レートで＠
-                      </Text>
-                      <Text color={white} fontSize={11}>
-                        0.02300000
-                      </Text>
-                      <Text color={white} fontSize={11}>
-                        相当
-                      </Text>
-                    </HStack>
-                  </VStack>
-                </HStack>
-              </Button>
-            </Link>
-            <Link href="/schedule-edit" asChild>
-              <Button h="auto" w="100%" bg={lightGrey} rounded="$lg">
-                <HStack w="$80" justifyContent="space-between" alignItems="center" px="$2" py="$5">
-                  <Box>
-                    <HStack space="xs" alignItems="center">
-                      <Icon as={CheckCircleIcon} size="sm" color={green} />
-                      <Text color={white} fontSize={19} bold pb="$1">
-                        bitFlyer
-                      </Text>
-                    </HStack>
-                    <HStack space="xs">
-                      <Text color={unclearWhite} fontSize={13}>
-                        毎日
-                      </Text>
-                      <Text color={unclearWhite} fontSize={13}>
-                        -
-                      </Text>
-                      <Text color={unclearWhite} fontSize={13}>
-                        18:00
-                      </Text>
-                    </HStack>
-                  </Box>
-                  <VStack space="xs" alignItems="flex-end">
-                    <HStack>
-                      <Text color={white} fontSize={19}>
-                        20,000
-                      </Text>
-                      <Text color={white} fontSize={13} pl="$1">
-                        円
-                      </Text>
-                    </HStack>
-                    <HStack space="xs">
-                      <Text color={white} fontSize={11}>
-                        現在レートで＠
-                      </Text>
-                      <Text color={white} fontSize={11}>
-                        0.56700000
-                      </Text>
-                      <Text color={white} fontSize={11}>
-                        相当
-                      </Text>
-                    </HStack>
-                  </VStack>
-                </HStack>
-              </Button>
-            </Link>
-            <Link href="/schedule-edit" asChild>
-              <Button h="auto" w="100%" bg="#33333366" rounded="$lg">
-                <HStack w="$80" justifyContent="space-between" alignItems="center" px="$2" py="$5">
-                  <Box>
-                    <HStack space="xs" alignItems="center">
-                      <Icon as={CloseCircleIcon} size="sm" color={red} />
-                      <Text color={white} fontSize={19} bold pb="$1">
-                        GMOコイン
-                      </Text>
-                    </HStack>
-                    <Text>停止中</Text>
-                  </Box>
-                  <VStack space="xs" alignItems="flex-end">
-                    <HStack>
-                      <Text color={white} fontSize={19}>
-                        50,000
-                      </Text>
-                      <Text color={white} fontSize={13} pl="$1">
-                        円
-                      </Text>
-                    </HStack>
-                    <HStack space="xs">
-                      <Text color={white} fontSize={11}>
-                        現在レートで＠
-                      </Text>
-                      <Text color={white} fontSize={11}>
-                        0.78900000
-                      </Text>
-                      <Text color={white} fontSize={11}>
-                        相当
-                      </Text>
-                    </HStack>
-                  </VStack>
-                </HStack>
-              </Button>
-            </Link>
-          </VStack>
-        </ScrollView>
+      <FlatList data={plans} renderItem={renderItem as any} keyExtractor={(item) => (item as Plan).id} p="$4" />
 
-        <HStack justifyContent="space-between" alignItems="center" p="$4" mb="$3" borderTopWidth={0.3} borderColor={unclearWhite} bg={darkGrey}>
-          <Text color={white}>積立プランを追加しよう</Text>
-          <Link href="/schedule-registration" asChild>
-            <Button w="$24" size="lg" variant="solid" action="primary" isDisabled={false} isFocusVisible={false} rounded="$full">
-              <ButtonText>追加</ButtonText>
-            </Button>
-          </Link>
-        </HStack>
-      </Box>
+      <HStack justifyContent="space-between" alignItems="center" p="$4" borderTopWidth={0.3} borderColor={unclearWhite} bg={darkGrey}>
+        <Text color={white}>積立プランを追加しよう</Text>
+        <Link href="/schedule-registration" asChild>
+          <Button w="$24" size="lg" variant="solid" action="primary" isDisabled={false} isFocusVisible={false} rounded="$full">
+            <ButtonText>追加</ButtonText>
+          </Button>
+        </Link>
+      </HStack>
     </Box>
   );
 }
